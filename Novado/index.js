66805eaf3085e655de258871c4179fdb4d225f22
IkkaJabua <156788844+IkkaJabua@users.noreply.github.com>
2024-03-28 15:30:53 +0400

    Titkmis davasrule
    Novado davamate Js

---
+++ Novado/index.js
@@ -0,0 +1,50 @@
+const taskInput = document.querySelector('.inputs__input-text');
+const taskAddButton = document.querySelector('.inputs__add');
+const taskList = document.querySelector('.tasks-list')
+const toDo = document.getElementById('toDo')
+const done = document.getElementById('done')
+
+let toDoCount = 0
+let doneCount = 0;
+
+function addTask() {
+    if (taskInput.value !== '') {
+        toDoCount++;
+
+        taskList.innerHTML += `
+        <li class="tasks-list__task">
+        <div class="task-list__task-left">
+          <input type="checkbox" class="task-list__done check" onchange="checkTask(event)" />
+          <p>${taskInput.value}</p>
+        </div>
+        <i class="fa-regular fa-trash-can task-list__delete" onclick="deleteTask()"></i>
+      </li>
+        `;
+        toDo.innerHTML = toDoCount;
+        done.innerHTML = `${doneCount} / ${toDoCount}`
+    }
+}
+
+function checkTask(e) {
+  console.log('hi');
+  if (e.target.checked === true) {
+    doneCount++
+    done.innerHTML = `${doneCount} / ${toDoCount}`
+  } else {
+    doneCount--
+    done.innerHTML = `${doneCount} / ${toDoCount}`
+  }
+}
+
+function deleteTask() {
+  let tasks = document.querySelectorAll('.tasks-list__task')
+}
+
+taskAddButton.addEventListener('click', () => addTask())
+
+taskInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        addTask()
+    }
+})
+
